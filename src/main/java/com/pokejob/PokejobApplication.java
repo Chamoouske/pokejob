5e10bf8425fa11413cabc7e06342edeb5094e4af
Ajax Lima <ajaxlima94@gmail.com>
2024-06-27 22:10:59 -0300

    feat: inicio do projeto

---
+++ src/main/java/com/pokejob/PokejobApplication.java
@@ -0,0 +1,13 @@
+package com.pokejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokejobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokejobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokejob/PokejobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokejob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokejobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
