ca92a7bf4ea3b0427a6b68ac968059021a053939
Ajax Lima <ajaxlima94@gmail.com>
2024-07-07 11:20:22 -0300

    refactor: utilizacao do dto no client

--- src/main/java/com/pokejob/pokemon/client/PokemonClient.java
+++ src/main/java/com/pokejob/pokemon/client/PokemonClient.java
@@ -1,7 +1,8 @@
 package com.pokejob.pokemon.client;
 
 import com.pokejob.config.FeignConfig;
-import com.pokejob.pokemon.dto.Pokemon;
+import com.pokejob.pokemon.dto.PokemonDto;
+import com.pokejob.pokemon.dto.ResultAllPokemonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "pokemonClient", url = "${servico.pokeapi.url-pokemon}", configuration = FeignConfig.class)
 public interface PokemonClient {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    ResultAllPokemonClient getAllLinksPokemon();
     @GetMapping(value = "{nomePokemon}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    Pokemon getPokemon(@PathVariable("nomePokemon") String nomePokemon);
+    PokemonDto getPokemon(@PathVariable("nomePokemon") String nomePokemon);
 }
